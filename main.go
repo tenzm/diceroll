f848dfd6c97fd7ecf163f0fa4b881bf4a19ff96e
Блинов Максим <tenzm@mail.ru>
2019-10-16 00:18:02 +0300

    added -norepeat

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ var (
 	start = flag.Int("start", 1, "minimum value of random")
 	end   = flag.Int("end", 6, "maximum value of random")
 	n     = flag.Int("n", 1, "number of repeat")
+	norepeat = flag.Bool("norepeat", false, "May repeat?")
 )
 
 func abs(a int) int {
@@ -34,6 +35,7 @@ func randInterval(l, r int) int {
 }
 
 func main() {
+	var repeated[100000] bool
 	flag.Parse()
 	if *seed == 0 {
 		rand.Seed(time.Now().Unix())
@@ -44,10 +46,25 @@ func main() {
 	if *start > *end {
 		fmt.Println("Ошибка! значение start должно быть меньше, чем значение end.")
 	} else {
+		if (*norepeat && *n > *end-*start + 1){
+			fmt.Println("Ошибка! Не возможно вывести", *n, "чисел в диапазоне от", *start,"до", *end,"без повторений.")
+			return
+		}
 		for i := 0; i < *n; i++ {
-			fmt.Print(randInterval(*start, *end))
-			if i != *n-1 {
-				fmt.Print(", ")
+			val := randInterval(*start, *end)
+			if *norepeat && repeated[val] != true{
+				repeated[val] = true
+				fmt.Print(val)
+				if i != *n-1 {
+					fmt.Print(", ")
+				}
+			} else if *norepeat {
+				i--
+			} else {
+				fmt.Print(val)
+				if i != *n-1 {
+					fmt.Print(", ")
+				}
 			}
 		}
 		fmt.Print("\n")
